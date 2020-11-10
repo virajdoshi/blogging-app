e1c95b102e19540dd8e9b230f523c9249eace754
virajdoshi <44646580+virajdoshi@users.noreply.github.com>
2020-11-10 10:31:16 +0530

    Added unfollow profile function

--- models/profile.js
+++ models/profile.js
@@ -33,5 +33,20 @@ module.exports = {
                 callback("Message: You are already following " + uid);
             }
         })
+    },
+
+    unfollow: function(con, payload, uid, callback){
+        con.query("select * from following where user_id in(select user_id from user_info where email = ?) AND following_user_id = ?", [payload.email, uid], function(err, result, fields){
+            if(result.length === 0){
+                callback("Message : You are already not following "+ uid + " OR profile does not exist.")
+            }
+            else{
+                con.query("delete from following where user_id in(select user_id from user_info where email = ?) and following_user_id = ?", [payload.email, uid], function(err, result, fields){
+                    con.query("select user_info.user_id, user_info.bio, count(following.following_user_id) as following from user_info inner join following on user_info.user_id = following.user_id where user_info.email = ?", payload.email, function(err, result, fields){
+                        callback({user_id : result[0].user_id, bio : result[0].bio, following : result[0].following})
+                    })
+                })
+            }
+        })
     }
 }
